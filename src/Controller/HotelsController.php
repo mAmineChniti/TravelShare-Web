f973130c1aa19f3319d3bbdb9fa9576641540e31
BOUHREM <bouhrem4@gmail.com>
2025-04-16 16:08:59 +0100

    e chi5aa

--- src/Controller/HotelsController.php
+++ src/Controller/HotelsController.php
@@ -2,13 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Hotels;
 use App\Repository\HotelsRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Hotels;
 
 class HotelsController extends AbstractController
 {
@@ -28,20 +27,25 @@ class HotelsController extends AbstractController
         ]);
     }
 
-    #[Route('/hotel/{id}', name: 'hotel_show')]
-    public function showHotel(Hotels $hotel): Response
+    #[Route('/dashboard/hotels', name: 'app_dashboard_hotels')]
+    public function listHotelsA(HotelsRepository $hotelsRepository): Response
     {
-        return $this->render('hotels/show.html.twig', [
-            'hotel' => $hotel,
+        $hotels = $hotelsRepository->findAll();
+        foreach ($hotels as $hotel) {
+            if ($hotel->getImageH()) {
+                $hotel->base64Image = base64_encode(stream_get_contents($hotel->getImageH()));
+            } else {
+                $hotel->base64Image = null;
+            }
+        }
+        return $this->render('dashboard/hotels.html.twig', [
+            'hotels' => $hotels,
         ]);
     }
 
-    #[Route('/hotel/add', name: 'hotel_add')]
-    public function addHotel(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/dashboard/hotel/add', name: 'hotel_add')]
+    public function addHotel(Request $request, HotelsRepository $hotelsRepo): Response
     {
-        if (!$this->isGranted('ROLE_ADMIN')) {
-            return $this->redirectToRoute('app_hotels');
-        }
 
         if ($request->isMethod('POST')) {
             $hotel = new Hotels();
@@ -49,14 +53,11 @@ class HotelsController extends AbstractController
             $hotel->setAdress($request->request->get('adress'));
             $hotel->setTelephone($request->request->get('telephone'));
             $hotel->setCapaciteTotale((int) $request->request->get('capacite_totale'));
-
-            $entityManager->persist($hotel);
-            $entityManager->flush();
-
+            $hotelsRepo->add($hotel);
             return $this->redirectToRoute('app_hotels');
         }
 
-        return $this->render('hotels/add.html.twig');
+        return $this->render('dashboard/addhotel.html.twig');
     }
 
     #[Route('/hotel/edit/{id}', name: 'hotel_edit')]
@@ -95,4 +96,5 @@ class HotelsController extends AbstractController
 
         return $this->redirectToRoute('app_hotels');
     }
+    
 }
